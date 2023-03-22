18c09d9d5e9e6223b8eebbc66bef2fd58764b4be
stevenwh <stevenwh@purdue.edu>
2023-03-22 09:43:55 -0400

    Update train

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from torch.utils.tensorboard import SummaryWriter
 from model import YoloMulti
 from bdd100k import BDD100k
 #from utils import non_max_supression, mean_average_precission, intersection_over_union
-from loss import MultiLoss
+from loss import MultiLoss, SegmentationLoss, DetectionLoss
 
 import matplotlib.pyplot as plt
 import tqdm
@@ -36,7 +36,8 @@ def parse_arg():
     parser.add_argument('--device', type=str, default="cuda", help='cuda or cpu')
     parser.add_argument('--batch', type=int, default=16, help='batch size')
     parser.add_argument('--epoch', type=int, default=100, help='epochs')
-    parser.add_argument('--num_workers', type=int, default=2, help='number of workers')
+    parser.add_argument('--num_workers', type=int, default=12, help='number of workers')
+    # Fix root below
     parser.add_argument('--root', type=str, default='/home/pumetu/Purdue/LaneDetection/BDD100k/', help='root directory for both image and labels')
     parser.add_argument('--cp', '-checkpoint', type=str, default='', help='path to checpoint of pretrained model')
     return parser.parse_args()
@@ -48,8 +49,10 @@ def main():
     model = YoloMulti().to(device)
 
     #Set optimizer and loss function
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
-    loss_fn = MultiLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-4) # Can try 6e-4
+    
+    #loss_fn = MultiLoss()
+    loss_fn = SegmentationLoss()
 
     transform = transforms.Compose([
         transforms.Resize((384, 640), interpolation=transforms.InterpolationMode.NEAREST),
@@ -67,24 +70,28 @@ def main():
                                 num_workers=args.num_workers,
                                 shuffle=False)
                   
-    for epoch in tqdm.tqdm(range(300)):
+    imgs, det, lane, drivable = next(iter(train_loader)) # First batch
+    model.train()
+
+    for epoch in tqdm.tqdm(range(args.epoch)):
         #--------------------------------------------------------------------------------------
         #Train
-        for imgs, det, lane, drivable in train_loader:
-            imgs, lane, drivable = imgs.to(device), lane.to(device), drivable.to(device)  
-            model.train()
-            running_loss = 0
+        
+        #for imgs, det, lane, drivable in train_loader:
+        imgs, lane, drivable = imgs.to(device), lane.to(device), drivable.to(device)  
+        model.train()
+        running_loss = 0
 
-            pdet, pseg = model(imgs)
-            lane = lane.squeeze(dim=2)
-            loss = loss_fn(pdet, pseg, det, lane, drivable)
+        pdet, pseg = model(imgs)
+        lane = lane.squeeze(dim=2)
+        loss = loss_fn(pseg, torch.stack(lane, drivable, axis=1))
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            running_loss += loss.item()
-            writer.add_scalar("Loss/train", running_loss, epoch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        writer.add_scalar("Loss/train", running_loss, epoch)
 
         writer.flush()
     
