0d9587701b9c1f20381ec6a08ed561535b011298
wstevens-11 <wstevens_11@outlook.com>
2023-04-02 12:54:20 -0400

    Finished segmentation postprocessing

---
+++ CutMix.py
@@ -0,0 +1,80 @@
+'''
+CutMix Data Augmentation  Implementation
+- Author: William Stevens
+Inspired by the official CutMix paper (https://arxiv.org/pdf/1905.04899.pdf)
+'''
+
+import torch
+import numpy as np
+
+class CutMix():
+    def __init__(self, image_width, image_height, num_classes, alpha=1):
+        '''
+        Initialize CutMix object
+        Params:
+        - image_width (int): width of training images
+        - image_height (int): height of training images
+        - num_classes (int): number of classes in training dataset
+        - alpha (float): the "combination ratio", as defined in CutMix paper (default to 1)
+
+        Initialize instance variables
+        Define hadamard function call as element-wise multiplication
+        '''
+        self.w = image_width
+        self.h = image_height
+        self.c = num_classes
+        self.a = alpha
+
+        self.hadamard = torch.mul()
+
+    
+    def get_cutmix(self, im1, im2, lab1, lab2):
+        '''
+        Obtain CutMix result of two input training images and their labels.
+        Params:
+        - im1 (Tensor): first training image, shape W x H x C
+        - im2 (Tensor): second training image, shape W x H x C
+        - lab1 (Tensor): label for first image, shape W x H x C
+        - lab2 (Tensor): label for second training image, shape W x H x C
+
+        Returns:
+        - im (Tensor): new training image after CutMix, shape W x H x C
+        - lab (Tensor): label for new training image after CutMix, shape W x H x C
+        '''
+
+        # Initialize m tensor and patch tensor as full of ones, shape W x H
+        m = torch.ones((self.w, self.h))
+        patch = torch.ones((self.w, self.h))
+
+        # Compute lambda from uniform distribution (same as beta distribution Beta(alpha, alpha))
+        # Will get value in between 0 and 1
+        lam = np.random.beta(self.a, self.a)
+
+        # Compute patch bounding box coordinates: Px, Py, Pw, Ph. From paper:
+        # Px: random int from 0 to W
+        # Py: random int from 0 to H
+        # Pw: W * sqrt(1 - lambda)
+        # Ph: H * sqrt(1 - lambda)
+        Px = np.random.randint(self.w)
+        Py = np.random.randint(self.h)
+        Pw = int(self.w * np.sqrt(1 - lam))
+        Ph = int(self.h * np.sqrt(1 - lam))
+
+        # Fill patch tensor with zeros outside of the patch bounding box computed above
+        # Bitwise XOR with original mask M to get patch mask
+        patch[0:self.w,0:Py] = 0
+        patch[0:self.w,Py+Ph:self.h] = 0
+        patch[0:Px,Py:Py+Ph] = 0
+        patch[Px:Px:Pw,Py:Py+Ph] = 0
+        m = torch.bitwise_xor(m, patch)
+
+        # Compute new cutmix training image by using hadamard product of m and im1, plus 1-m hadamard im2
+        im = (self.hadamard(m, im1)) + (self.hadamard(1-m, im2))
+
+        # Compute corresponding training label by using lambda * label1 + (1 - lambda) * label2
+        lab = lam * lab1 + (1 - lam) * lab2
+
+        return im, lab
+
+
+    
--- segmentation_postprocess.py
+++ segmentation_postprocess.py
@@ -1,14 +1,72 @@
 import torch
 import matplotlib.pyplot as plt
+from PIL import Image
+import torchvision.transforms as transforms
+import numpy as np
 
-imgs = torch.load('imgs.pt')
-seg = torch.load('seg.pt')
-pseg = torch.load('pseg.pt')
 
-print(imgs[0][0].shape)
-print(seg.shape)
-print(pseg.shape)
+color_conversions = {
+    0: (255,200,0), # Crosswalk
+    1: (30,30,255), 
+    2: (30,30,255),
+    3: (150,250,0), # Single yellow
+    4: (255,50,100), # Road curb
+    5: (30,30,255),
+    6: (30,255,100), # Single white
+    8: (30,30,255),
+    9: (250,128,114),
+    10: (0,206,209)
+}
 
-#gt1 = torch.argmax(pseg[0], dim=0).detach().cpu().numpy()
-#plt.imshow(gt1, cmap='gray')
-#plt.savefig("gt1.png")
+transform = transforms.ToPILImage()
+
+imgs = torch.load('imgs.pt').clone()
+seg = torch.load('seg.pt').clone()
+pseg = torch.load('pseg.pt').clone()
+    
+seg[seg==1] = 255
+pseg[pseg==1] = 255
+
+image_num = 13
+
+img13 = transform(imgs[image_num])
+img13.save("img13.png")
+
+
+# ----------- Lane Lines -----------
+lane13 = img13.copy()
+for lane in range(9):
+    if (lane == 7): continue # Do not add mask for background lane
+    mask = transform(np.uint8(seg[image_num][lane].detach().numpy())).convert('RGBA')
+
+    data = np.array(mask)
+    red, green, blue, alpha = data.T
+    white_areas = (red == 255) & (green == 255) & (blue == 255)
+    data[..., :-1][white_areas.T] = color_conversions[lane]
+    mask = Image.fromarray(data)
+
+    alpha = mask.copy()
+    alpha = alpha.convert('L')
+    alpha.point(lambda p: 128 if p > 0 else 0)
+    lane13.paste(mask, mask=alpha)
+
+lane13.save("lane13.png")
+
+# ----------- Drivable Area -----------
+drivable13 = img13.copy()
+for drive in range(9,11):
+    mask = transform(np.uint8(seg[image_num][drive].detach().numpy())).convert('RGBA')
+
+    data = np.array(mask)
+    red, green, blue, alpha = data.T
+    white_areas = (red == 255) & (green == 255) & (blue == 255)
+    data[..., :-1][white_areas.T] = color_conversions[drive]
+    mask = Image.fromarray(data)
+    mask.putalpha(128)
+
+    alpha = mask.copy()
+    alpha = alpha.convert('L')
+    alpha.point(lambda p: 128 if p > 0 else 0)
+    drivable13.paste(mask, mask=alpha)
+
+drivable13.save("drivable13.png")
